c50a5b2febd7b1a459e55522d83b838b22887c34
Xander Miller <xander.miller@gmail.com>
2013-10-24 20:21:08 -0400

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WeSay::Application.config.secret_key_base = '5b674b5c01aa405c93cdfc0c471c65b39988a3f7dc9b98a0493eccf1baee3fe2b8d5fba7d37304adad0254c68ae42b7d51b277925730fc1cbf862c7b9b433857'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WeSay::Application.config.session_store :cookie_store, key: '_we_say_session'
